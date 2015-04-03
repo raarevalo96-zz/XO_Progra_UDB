a5927016b50d0dbbfa87410ea0eb2b434d6d12e7
raarevalo96 <rodrigo.arevalo4@gmail.com>
2015-04-03 11:03:26 -0600

    Reimplementación del modo de dos jugadores

--- Juego/main.cpp
+++ Juego/main.cpp
@@ -7,29 +7,16 @@ using namespace std;
 
 // Prototipo de funciones, la explicación de cada una está en la definición de cada una, debajo del main.
 void limpiar(){system("cls");}
-void llenar(char tablero[3][3]);
+void turno_juego();
 void mostrar(char tablero[3][3]);
-bool verificar(char tablero);
+bool fin_juego();
+
+char turno='X';
+char tablero[3][3]={{'1','2','3'},{'4','5','6'},{'7','8','9'}};
+bool empate=false;
 
 int main(){
-    /*
-        Variables a utilizar:
-            Tipo entero:
-                gamemode = opción del menu principal.
-                turno = determina el caracter a ingresar (X,O).
-                fila y columna = posiciones del caracter en el arreglo.
-                nuevo = determina si el programa seguirá corriendo luego de su ejecución completa.
-            Tipo char:
-                tablero[3][3] = arreglo bidimensional para el tablero.
-            Tipo bool:
-                noerror = determina si en la posición no hay un caracter insertado, se usa en conjunto con la función verificar.
-    */
-
-    int gamemode, turno, fila, columna, nuevo;
-    char tablero[3][3];
-    bool noerror;
-
-    llenar(tablero);
+    int gamemode, nuevo=1;
 
     do{
         limpiar();
@@ -57,6 +44,12 @@ int main(){
             system("PAUSE");
 
             // PENDIENTE: Reimplementación
+            do{
+                limpiar();
+                mostrar(tablero);
+                turno_juego();
+                fin_juego();
+            } while (!fin_juego);
 
             cout<<"Inserta 1 si deseas salir del juego, cualquier otro valor para jugar de nuevo."<<endl;
             cout<<"Ingresa una opcion: ";
@@ -65,6 +58,7 @@ int main(){
 
         // Salir del juego:
         case 3:
+            return 0;
             break;
 
         // Si se inserta un dato no esperado:
@@ -74,39 +68,78 @@ int main(){
             limpiar();
             break;
         }
-    } while (nuevo!=1 || gamemode!=3);
+    } while (nuevo!=1);
     system("PAUSE");
     return 0;
 }
-
-// Una vez declarado el arreglo para el tablero, la funcion llenar se encarga de asignar el caracter '-' a cada celda.
-
-void llenar(char tablero[3][3]){
-    for(int i=0;i<3;i++){
-        for(int j=0;j<3;j++){
-            tablero[i][j]='-';
-        }
-    }
-}
-
 
 // Función para mostrar el tablero.
 void mostrar(char tablero[3][3]){
     int j=0;
     for(int i=0;i<3;i++){
-        cout<<i+1<<"\t";
         for(j=0;j<3;j++){
-            cout<<tablero[i][j]<<"\t";
+            cout<<"\t"<<tablero[i][j];
         }
         cout<<endl<<endl;
     }
 }
 
-// Verifica que en la celda no se encuentre ningun caracter 'X' o 'O', para evitar sobreescribir.
-bool verificar(char tablero){
-    if(tablero=='-'){
-        return 1;
-    } else {
-        return 0;
+void turno_juego()
+{
+    int eleccion;
+    int fila = 0, columna = 0;
+
+    if (turno == 'X'){
+        cout << "Es el turno del jugador 1 [X]: ";
+    } else if (turno == 'O'){
+        cout << "Es el turno del jugador 2 [O]: ";
     }
+
+    cin>>eleccion;
+
+    switch (eleccion){
+        case 1: fila = 0; columna = 0; break;
+        case 2: fila = 0; columna = 1; break;
+        case 3: fila = 0; columna = 2; break;
+        case 4: fila = 1; columna = 0; break;
+        case 5: fila = 1; columna = 1; break;
+        case 6: fila = 1; columna = 2; break;
+        case 7: fila = 2; columna = 0; break;
+        case 8: fila = 2; columna = 1; break;
+        case 9: fila = 2; columna = 2; break;
+        default: cout << "Opcion incorrecta, intenta de nuevo!"<<endl;
+        turno_juego();
+    }
+
+    if (turno == 'X' && tablero[fila][columna] != 'X' && tablero[fila][columna] != 'O'){
+        tablero[fila][columna] = 'X';
+        turno = 'O';
+    } else if (turno == 'O' && tablero[fila][columna] != 'X' && tablero[fila][columna] != 'O'){
+        tablero[fila][columna] = 'O';
+        turno = 'X';
+    } else {
+        cout << "La celda que tomaste ya esta ocupada, intenta de nuevo!";
+        turno_juego();
+    }
+}
+
+bool fin_juego(){
+    // Buscar ganador
+    for (int i=0;i<3;i++){
+        if ((tablero[i][0] == tablero[i][1] && tablero[i][1] == tablero[i][2]) || (tablero[0][i] == tablero[1][i] && tablero[1][i] == tablero[2][i]) || (tablero[0][0] == tablero[1][1] && tablero[1][1] == tablero[2][2]) || (tablero[0][2] == tablero[1][1] && tablero[1][1] == tablero[2][0])){
+            return true;
+        }
+    }
+
+    //Buscar empate
+    for (int i = 0; i < 3; i++){
+        for (int j = 0; j < 3; j++){
+            if (tablero[i][j] != 'X' && tablero[i][j] != 'O'){
+                return false;
+            }
+        }
+    }
+
+    empate = true;
+    return true;
 }
