48006ce44bcab248b234031cb61ae33e7a5dec6a
raarevalo96 <rodrigo.arevalo4@gmail.com>
2015-04-06 13:32:15 -0600

    Implementación del modo de un jugador

--- Juego/main.cpp
+++ Juego/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cstring>
 #include <cstdlib>
+#include <time.h>
 
 using namespace std;
 
@@ -9,6 +10,7 @@ void limpiar(){system("cls");}
 void turno_juego();
 void mostrar(char tablero[3][3]);
 void llenar(char tablero[3][3]);
+void un_jugador();
 
 //Declaración de variables universales
 char turno='X';
@@ -35,8 +37,59 @@ int main(){
         // Un jugador:
         case 1:
             limpiar();
-                cout<<"Modo de un jugador."<<endl<<"Por defecto, jugaras con la X."<<endl;
-                cout<<"Funcion no implementada, disculpa las molestias."<<endl;
+                cout<<"Modo de un jugador."<<endl<<"Por defecto, jugaras con la X."<<endl<<endl;
+                turno='X';
+
+                do{
+                limpiar();
+                mostrar(tablero);
+                un_jugador();
+
+                //Buscar celdas vacias
+                for (int i=0;i<3;i++){
+                    for (int j=0;j<3;j++){
+                        if (tablero[i][j] != 'X' && tablero[i][j] != 'O'){
+                            fin_juego=false;
+                            empate=false;
+                            lleno=false;
+                        } else if (tablero[i][j]=='X' || tablero[i][j]=='O'){
+                            lleno=true;
+                        }
+                    }
+                }
+
+                //Buscar ganador
+                for (int i=0;i<3;i++){
+                    if ((tablero[i][0] == tablero[i][1] && tablero[i][1] == tablero[i][2]) || (tablero[0][i] == tablero[1][i] && tablero[1][i] == tablero[2][i]) || (tablero[0][0] == tablero[1][1] && tablero[1][1] == tablero[2][2]) || (tablero[0][2] == tablero[1][1] && tablero[1][1] == tablero[2][0])){
+                        fin_juego=true;
+                        if(turno=='X'){
+                            ganador='O';
+                        } else if(turno=='O'){
+                            ganador='X';
+                        }
+                    } else if(lleno){
+                        empate=true;
+                        fin_juego=true;
+                    }
+                }
+            } while (!fin_juego);
+
+            limpiar();
+            cout<<"El juego ha terminado!"<<endl;
+            if(empate){
+                cout<<"Es un empate"<<endl;
+                mostrar(tablero);
+            } else {
+                if (ganador=='X'){
+                    cout<<"Has ganado!"<<endl;
+                    mostrar(tablero);
+                } else if (ganador=='O'){
+                    cout<<"Has perdido!"<<endl;
+                    mostrar(tablero);
+                }
+            }
+
+
                 cout<<"Inserta 1 si deseas salir del juego, cualquier otro valor para jugar de nuevo."<<endl;
                 cout<<"Ingresa una opcion: ";
                 cin>>nuevo;
@@ -48,6 +101,7 @@ int main(){
             cout<<"Modo de dos jugadores."<<endl;
             cout<<"El jugador 1 juega con X"<<endl<<"El jugador 2 juega con O"<<endl;
             system("PAUSE");
+            turno='X';
 
             do{
                 limpiar();
@@ -87,6 +141,7 @@ int main(){
             cout<<"El juego ha terminado!"<<endl;
             if(empate){
                 cout<<"Es un empate"<<endl;
+                mostrar(tablero);
             } else {
                 if (ganador=='X'){
                     cout<<"Ha ganado el jugador 1 [X]!"<<endl;
@@ -183,4 +238,45 @@ void llenar(char tablero[3][3]){
     tablero[2][0]='7';
     tablero[2][1]='8';
     tablero[2][2]='9';
+}
+
+void un_jugador(){
+    int eleccion;
+    int fila = 0, columna = 0;
+    srand(time(NULL));
+
+    if (turno == 'X'){
+        cout<<endl<<"Es tu turno [X]: ";
+        cin>>eleccion;
+    } else if (turno == 'O'){
+        eleccion = rand() % 9 + 1;
+    }
+
+    switch (eleccion){
+        case 1: fila = 0; columna = 0; break;
+        case 2: fila = 0; columna = 1; break;
+        case 3: fila = 0; columna = 2; break;
+        case 4: fila = 1; columna = 0; break;
+        case 5: fila = 1; columna = 1; break;
+        case 6: fila = 1; columna = 2; break;
+        case 7: fila = 2; columna = 0; break;
+        case 8: fila = 2; columna = 1; break;
+        case 9: fila = 2; columna = 2; break;
+        default:
+            if(turno=='X'){cout << "Opcion incorrecta, intenta de nuevo!"<<endl<<endl;}
+            un_jugador();
+    }
+
+    if (turno == 'X' && tablero[fila][columna] != 'X' && tablero[fila][columna] != 'O'){
+        tablero[fila][columna] = 'X';
+        turno = 'O';
+    } else if (turno == 'O' && tablero[fila][columna] != 'X' && tablero[fila][columna] != 'O'){
+        tablero[fila][columna] = 'O';
+        turno = 'X';
+    } else {
+        if(turno=='X'){cout<<"La celda que tomaste ya esta ocupada, intenta de nuevo!"<<endl;}
+        limpiar();
+        mostrar(tablero);
+        un_jugador();
+    }
 }
