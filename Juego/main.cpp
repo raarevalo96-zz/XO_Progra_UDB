531c95e89bbc38efe455639d97b05dad2b39e774
raarevalo96 <rodrigo.arevalo4@gmail.com>
2015-04-06 12:15:29 -0600

    Implementación del modo de dos jugadores

--- Juego/main.cpp
+++ Juego/main.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <cstring>
-#include <conio.h>
 #include <cstdlib>
 
 using namespace std;
@@ -9,17 +8,23 @@ using namespace std;
 void limpiar(){system("cls");}
 void turno_juego();
 void mostrar(char tablero[3][3]);
-bool fin_juego();
+void llenar(char tablero[3][3]);
 
+//Declaración de variables universales
 char turno='X';
-char tablero[3][3]={{'1','2','3'},{'4','5','6'},{'7','8','9'}};
+char ganador;
+char tablero[3][3];
 bool empate=false;
+bool fin_juego=false;
+bool lleno=false;
 
 int main(){
+    //Declaración de variables locales
     int gamemode, nuevo=1;
 
     do{
         limpiar();
+        llenar(tablero);
         cout<<"Bienvenido a Equis Cero."<<endl<<endl;
         cout<<"Escoge el modo de juego:"<<endl<<"1. Un jugador."<<endl<<"2. Dos jugadores."<<endl<<"3. Salir del juego."<<endl;
         cout<<endl<<"Ingresa una opcion: ";
@@ -31,6 +36,7 @@ int main(){
         case 1:
             limpiar();
                 cout<<"Modo de un jugador."<<endl<<"Por defecto, jugaras con la X."<<endl;
+                cout<<"Funcion no implementada, disculpa las molestias."<<endl;
                 cout<<"Inserta 1 si deseas salir del juego, cualquier otro valor para jugar de nuevo."<<endl;
                 cout<<"Ingresa una opcion: ";
                 cin>>nuevo;
@@ -43,13 +49,53 @@ int main(){
             cout<<"El jugador 1 juega con X"<<endl<<"El jugador 2 juega con O"<<endl;
             system("PAUSE");
 
-            // PENDIENTE: Reimplementación
             do{
                 limpiar();
                 mostrar(tablero);
                 turno_juego();
-                fin_juego();
+
+                //Buscar celdas vacias
+                for (int i=0;i<3;i++){
+                    for (int j=0;j<3;j++){
+                        if (tablero[i][j] != 'X' && tablero[i][j] != 'O'){
+                            fin_juego=false;
+                            empate=false;
+                            lleno=false;
+                        } else if (tablero[i][j]=='X' || tablero[i][j]=='O'){
+                            lleno=true;
+                        }
+                    }
+                }
+
+                //Buscar ganador
+                for (int i=0;i<3;i++){
+                    if ((tablero[i][0] == tablero[i][1] && tablero[i][1] == tablero[i][2]) || (tablero[0][i] == tablero[1][i] && tablero[1][i] == tablero[2][i]) || (tablero[0][0] == tablero[1][1] && tablero[1][1] == tablero[2][2]) || (tablero[0][2] == tablero[1][1] && tablero[1][1] == tablero[2][0])){
+                        fin_juego=true;
+                        if(turno=='X'){
+                            ganador='O';
+                        } else if(turno=='O'){
+                            ganador='X';
+                        }
+                    } else if(lleno){
+                        empate=true;
+                        fin_juego=true;
+                    }
+                }
             } while (!fin_juego);
+
+            limpiar();
+            cout<<"El juego ha terminado!"<<endl;
+            if(empate){
+                cout<<"Es un empate"<<endl;
+            } else {
+                if (ganador=='X'){
+                    cout<<"Ha ganado el jugador 1 [X]!"<<endl;
+                    mostrar(tablero);
+                } else if (ganador=='O'){
+                    cout<<"Ha ganado el jugador 2 [O]!"<<endl;
+                    mostrar(tablero);
+                }
+            }
 
             cout<<"Inserta 1 si deseas salir del juego, cualquier otro valor para jugar de nuevo."<<endl;
             cout<<"Ingresa una opcion: ";
@@ -90,9 +136,9 @@ void turno_juego()
     int fila = 0, columna = 0;
 
     if (turno == 'X'){
-        cout << "Es el turno del jugador 1 [X]: ";
+        cout<<endl<<"Es el turno del jugador 1 [X]: ";
     } else if (turno == 'O'){
-        cout << "Es el turno del jugador 2 [O]: ";
+        cout<<endl<<"Es el turno del jugador 2 [O]: ";
     }
 
     cin>>eleccion;
@@ -107,8 +153,9 @@ void turno_juego()
         case 7: fila = 2; columna = 0; break;
         case 8: fila = 2; columna = 1; break;
         case 9: fila = 2; columna = 2; break;
-        default: cout << "Opcion incorrecta, intenta de nuevo!"<<endl;
-        turno_juego();
+        default:
+            cout << "Opcion incorrecta, intenta de nuevo!"<<endl<<endl;
+            turno_juego();
     }
 
     if (turno == 'X' && tablero[fila][columna] != 'X' && tablero[fila][columna] != 'O'){
@@ -118,28 +165,22 @@ void turno_juego()
         tablero[fila][columna] = 'O';
         turno = 'X';
     } else {
-        cout << "La celda que tomaste ya esta ocupada, intenta de nuevo!";
+        cout<<"La celda que tomaste ya esta ocupada, intenta de nuevo!"<<endl;
+        system("PAUSE");
+        limpiar();
+        mostrar(tablero);
         turno_juego();
     }
 }
 
-bool fin_juego(){
-    // Buscar ganador
-    for (int i=0;i<3;i++){
-        if ((tablero[i][0] == tablero[i][1] && tablero[i][1] == tablero[i][2]) || (tablero[0][i] == tablero[1][i] && tablero[1][i] == tablero[2][i]) || (tablero[0][0] == tablero[1][1] && tablero[1][1] == tablero[2][2]) || (tablero[0][2] == tablero[1][1] && tablero[1][1] == tablero[2][0])){
-            return true;
-        }
-    }
-
-    //Buscar empate
-    for (int i = 0; i < 3; i++){
-        for (int j = 0; j < 3; j++){
-            if (tablero[i][j] != 'X' && tablero[i][j] != 'O'){
-                return false;
-            }
-        }
-    }
-
-    empate = true;
-    return true;
+void llenar(char tablero[3][3]){
+    tablero[0][0]='1';
+    tablero[0][1]='2';
+    tablero[0][2]='3';
+    tablero[1][0]='4';
+    tablero[1][1]='5';
+    tablero[1][2]='6';
+    tablero[2][0]='7';
+    tablero[2][1]='8';
+    tablero[2][2]='9';
 }
